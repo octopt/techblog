817f0a9aac58bb9a37d5a5f58fcd467cdba60d3e
todoroki <yasu.todo@gmail.com>
2020-04-04 17:35:40 +0900

    update

--- seq2seq/main.py
+++ seq2seq/main.py
@@ -10,23 +10,62 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 
-MAX_LENGTH = 10
 SOS_token = 0
 EOS_token = 1
 
 device = "cuda" # torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class Lang:
-    def __init__( self, name ):
-        self.name = name
+    def __init__( self, filename ):
+        self.filename = filename
         self.word2index = {}
         self.word2count = {}
+        self.sentences = []
         self.index2word = { 0: "SOS", 1: "EOS" }
         self.n_words = 2  # Count SOS and EOS
 
+        with open( self.filename ) as fd:
+            for i, line in enumerate( fd.readlines() ):
+                line = line.strip()
+                self.sentences.append( line )
+        self.allow_list = [ True ] * len( self.sentences )
+        self.target_sentences = self.sentences[ :: ]
+                
+    def get_sentences( self ):
+        return self.sentences[ :: ] 
+
+    def get_sentence( self, index ):
+        return self.sentences[ index ]
+
+    def choice( self ):
+        while True:
+            index = random.randint( 0, len( self.allow_list ) - 1 )
+            if self.allow_list[ index ]:
+                break
+        return self.sentences[ index ], index
+
+    def get_allow_list( self, max_length ):
+        allow_list = []
+        for sentence in self.sentences:
+            if len( sentence.split() ) < max_length:
+                allow_list.append( True )
+            else:
+                allow_list.append( False )
+        return allow_list
+                    
+    def load_file( self, allow_list = [] ):
+        if allow_list:
+            self.allow_list = [x and y for (x,y) in zip( self.allow_list, allow_list ) ]
+        self.target_sentences = []
+        for i, sentence in enumerate( self.sentences ):
+            if self.allow_list[ i ]:
+                self.addSentence( sentence )
+                self.target_sentences.append( sentence )
+                    
     def addSentence( self, sentence ):
-        for word in sentence.split(' '):
+        for word in sentence.split():
             self.addWord(word)
+            
 
     def addWord( self, word ):
         if word not in self.word2index:
@@ -36,49 +75,6 @@ class Lang:
             self.n_words += 1
         else:
             self.word2count[word] += 1        
-
-def unicodeToAscii(s):
-    return ''.join(
-        c for c in unicodedata.normalize('NFD', s)
-        if unicodedata.category(c) != 'Mn'
-    )        
-
-def normalizeString(s):
-    s = unicodeToAscii(s.lower().strip())
-    s = re.sub(r"([.!?])", r" \1", s)
-    s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
-    return s        
-        
-def readLangs(lang1, lang2, reverse=False):
-    lines = open('%s-%s.txt' % (lang1, lang2), encoding='utf-8').read().strip().split('\n')
-    pairs = [ [ normalizeString(s) for s in l.split('\t') ] for l in lines]
-    if reverse:
-        pairs = [list(reversed(p)) for p in pairs]
-        input_lang = Lang(lang2)
-        output_lang = Lang(lang1)
-    else:
-        input_lang = Lang(lang1)
-        output_lang = Lang(lang2)
-    return input_lang, output_lang, pairs
-
-def prepareData(lang1, lang2, reverse=False):
-    input_lang, output_lang, pairs = readLangs( lang1, lang2, reverse )
-    pairs = filterPairs( pairs )
-    
-    for pair in pairs:
-        input_lang.addSentence( pair[0] )
-        output_lang.addSentence( pair[1] )
-    print( "Counted words:" )
-    print( input_lang.name, input_lang.n_words )
-    print( output_lang.name, output_lang.n_words )
-    return input_lang, output_lang, pairs
-
-def filterPair( p ):
-    eng_prefixes = ("i am ", "i m ", "he is", "he s ", "she is", "she s ", "you are", "you re ", "we are", "we re ", "they are", "they re " )
-    return ( len(p[0].split(' ')) < MAX_LENGTH ) and ( len(p[1].split(' ')) < MAX_LENGTH  ) and p[1].startswith( eng_prefixes )
-
-def filterPairs(pairs):
-    return [ pair for pair in pairs if filterPair( pair )]
 
 # Start core part
 class Encoder( nn.Module ):
@@ -120,26 +116,35 @@ def tensorFromSentence( lang, sentence ):
     indexes.append( EOS_token )
     return torch.tensor( indexes, dtype=torch.long ).to( device ).view(-1, 1)
 
-def tensorsFromPair(pair, input_lang, output_lang):
-    input_tensor = tensorFromSentence( input_lang, pair[ 0 ])
-    output_tensor = tensorFromSentence( output_lang, pair[ 1 ] )
+def tensorsFromPair( input_lang, output_lang ):
+    input_sentence, index = input_lang.choice()
+    output_sentence = output_lang.get_sentence( index )
+    
+    input_tensor = tensorFromSentence( input_lang, input_sentence )
+    output_tensor = tensorFromSentence( output_lang, output_sentence )
     return (input_tensor, output_tensor)
 
 def main():
-    n_iters = 75000
-    learning_rate=0.01
-    hidden_size = 256
+    n_iters       = 75000
+    learning_rate = 0.01
+    hidden_size   = 256
+    max_length = 10
+    input_lang = Lang( 'fr.txt' )
+    output_lang = Lang( 'en.txt')
     
-    input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
+    allow_list = [x and y for (x,y) in zip( input_lang.get_allow_list( max_length ), output_lang.get_allow_list( max_length ) ) ]
+            
+    input_lang.load_file( allow_list )
+    output_lang.load_file( allow_list )
+    
     
     encoder = Encoder( input_lang.n_words, hidden_size ).to( device )
     decoder = Decoder( hidden_size, output_lang.n_words ).to( device )
     
     encoder_optimizer = optim.SGD( encoder.parameters(), lr=learning_rate )
     decoder_optimizer = optim.SGD( decoder.parameters(), lr=learning_rate )
-    
-    training_pairs = [ tensorsFromPair( random.choice( pairs ), input_lang, output_lang ) for i in range( n_iters ) ]
-                       
+
+    training_pairs = [ tensorsFromPair( input_lang, output_lang ) for i in range( n_iters ) ]
     criterion = nn.NLLLoss()
     
     for i in range( 1, n_iters + 1):
@@ -154,20 +159,20 @@ def main():
         output_length = output_tensor.size(0)
         
         # Encoder phese
-        for ei in range( input_length ):
-            encoder_output, encoder_hidden = encoder( input_tensor[ ei ], encoder_hidden )
+        for i in range( input_length ):
+            encoder_output, encoder_hidden = encoder( input_tensor[ i ], encoder_hidden )
             
         # Decoder phese
         loss = 0
         decoder_input  = torch.tensor( [ [ SOS_token ] ] ).to( device )
         decoder_hidden = encoder_hidden
-        for di in range( output_length ):
+        for i in range( output_length ):
             decoder_output, decoder_hidden = decoder( decoder_input, decoder_hidden )
-            decoder_input = output_tensor[ di ]
+            decoder_input = output_tensor[ i ]
             if random.random() < 0.5: 
                 topv, topi                     = decoder_output.topk( 1 )
                 decoder_input                  = topi.squeeze().detach() # detach from history as input
-            loss += criterion( decoder_output, output_tensor[ di ] ) 
+            loss += criterion( decoder_output, output_tensor[ i ] ) 
             if decoder_input.item() == EOS_token: break
             
         loss.backward()
