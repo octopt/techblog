168fbc698cfe6db5041d3ac6edc14aafe427daed
todoroki <yasu.todo@gmail.com>
2020-04-07 17:16:01 +0900

    add evaluation function for main.py

--- seq2seq/eval.py
+++ seq2seq/eval.py
@@ -10,6 +10,7 @@ EOS_token = 1
 device = 'cuda'
 
 def evaluate( sentence, max_length ):
+    use_attention = False
     input_lang  = Lang( 'jpn.txt')
     output_lang = Lang( 'eng.txt' )
     
@@ -29,13 +30,19 @@ def evaluate( sentence, max_length ):
     encoder.load_state_dict( torch.load( enfile ) )
     decoder.load_state_dict( torch.load( defile ) )
     
+    
+    
     with torch.no_grad():
         input_tensor   = tensorFromSentence(input_lang, sentence)
         input_length   = input_tensor.size()[0]
         encoder_hidden = encoder.initHidden()
+        if use_attention:
+            encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
         
         for ei in range(input_length):
             encoder_output, encoder_hidden = encoder(input_tensor[ei], encoder_hidden)
+            if use_attention:
+                encoder_outputs[ei] += encoder_output[0, 0]
 
         decoder_input      = torch.tensor([[SOS_token]], device=device)  # SOS
         decoder_hidden     = encoder_hidden
@@ -43,7 +50,10 @@ def evaluate( sentence, max_length ):
         decoder_attentions = torch.zeros(max_length, max_length)
         
         for di in range(max_length):
-            decoder_output, decoder_hidden = decoder( decoder_input, decoder_hidden )
+            if use_attention:
+                decoder_output, decoder_hidden, decoder_attention = decoder( decoder_input, decoder_hidden, encoder_outputs )
+            else:
+                decoder_output, decoder_hidden = decoder( decoder_input, decoder_hidden )
                 
             topv, topi = decoder_output.data.topk(1)
             if topi.item() == EOS_token:
